0ad0a9d9507073ae06c0e8480d61062fd94b6bf7
Trackerpjau <38476784+percyavaur@users.noreply.github.com>
2019-10-12 20:14:23 -0500

    Updates

---
+++ precache-manifest.f68108f8628d930cdfba5591e8cc2cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779438c8381fe26cb43345481df8d44c",
+    "url": "/movilperu/index.html"
+  },
+  {
+    "revision": "d61a11ccb7aa51492d71",
+    "url": "/movilperu/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "8a3014593ec7a183af97",
+    "url": "/movilperu/static/css/main.b421c2c7.chunk.css"
+  },
+  {
+    "revision": "d61a11ccb7aa51492d71",
+    "url": "/movilperu/static/js/2.87c479ca.chunk.js"
+  },
+  {
+    "revision": "8a3014593ec7a183af97",
+    "url": "/movilperu/static/js/main.8dc7f743.chunk.js"
+  },
+  {
+    "revision": "3c8d55ea248e1b027162",
+    "url": "/movilperu/static/js/runtime~main.69c700b3.js"
+  }
+]);
